d31b507d8fca863413105b5c5374ecf15e709356
Abdul Basit <abdulbasit5361234@gmail.com>
2023-05-27 13:42:55 +0500

    Fix web view

--- webApp/build.gradle.kts
+++ webApp/build.gradle.kts
@@ -3,8 +3,19 @@ plugins {
     id("org.jetbrains.compose")
 }
 
+val copyJsResources = tasks.create("copyJsResourcesWorkaround", Copy::class.java) {
+    dependsOn(":webApp:jsDevelopmentExecutableCompileSync")
+    from(project(":shared").file("src/commonMain/resources"))
+    into("build/processedResources/js/main")
+}
+
+afterEvaluate {
+    project.tasks.getByName("jsProcessResources").finalizedBy(copyJsResources)
+}
+
 kotlin {
     js(IR) {
+        moduleName = "CircularFillableLoader"
         browser()
         binaries.executable()
     }
@@ -14,6 +25,8 @@ kotlin {
                 implementation(project(":shared"))
                 implementation(npm("path-browserify", "^1.0.1"))
                 implementation(npm("os-browserify", "^0.3.0"))
+                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+                implementation(compose.components.resources)
             }
         }
     }
